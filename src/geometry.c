8bebbe401cef2309c08bbd3cff623c959572f3bb
nastyaNSK <nastavlasenkova@gmail.com>
2019-05-10 08:39:00 +0700

    Parser

---
+++ src/geometry.c
@@ -0,0 +1,53 @@
+#include "geometry.h"
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TRIANGLE 1
+#define CIRCLE 2
+
+int Parser(Figure* new, char* A)
+
+{
+    char B[256];
+    int i = 0;
+    while (A[i] >= 'a' && A[i] <= 'z') {
+        B[i] = A[i];
+
+        i++;
+    }
+
+    B[i] = '\0';
+    if (!(strcmp(B, "triangle"))) {
+        new->type = TRIANGLE;
+    } else if (!(strcmp(B, "circle"))) {
+        new->type = CIRCLE;
+    } else {
+        printf("Unknown type\n");
+
+        return 1;
+    }
+
+    char* end;
+    end = A;
+    i = -1;
+    new->coordinates = (double*)malloc(sizeof(double) * 100);
+    if (new->coordinates == NULL) {
+        printf("Alloceted error");
+
+        return 1;
+    }
+
+    while (*A) {
+        new->coordinates[i] = strtod(A, &end);
+        A = end;
+        i++;
+        while (!(isdigit(*A) || *A == '-' || *A == '+') && *A) {
+            A++;
+        }
+    }
+    new->size = i;
+
+    return 0;
+}
