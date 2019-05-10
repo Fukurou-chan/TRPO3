25ac2a2427499ea6aa0bf28ddfaf0ab3efda2c5c
nastyaNSK <nastavlasenkova@gmail.com>
2019-05-10 09:04:16 +0700

    New function

--- src/geometry.c
+++ src/geometry.c
@@ -1,11 +1,47 @@
 #include "geometry.h"
 #include <ctype.h>
 #include <math.h>
-
-void S_And_P_Circle(double* S, double* P, Figure* new)
+int Parser(Figure* new, char* A)
 
 {
-    double r = new->coordinates[2];
-    *S = M_PI * (r * r);
-    *P = 2 * M_PI * r;
+    char B[256];
+    int i = 0;
+    while (A[i] >= 'a' && A[i] <= 'z') {
+        B[i] = A[i];
+
+        i++;
+    }
+
+    B[i] = '\0';
+    if (!(strcmp(B, "triangle"))) {
+        new->type = TRIANGLE;
+    } else if (!(strcmp(B, "circle"))) {
+        new->type = CIRCLE;
+    } else {
+        printf("Unknown type\n");
+
+        return 1;
+    }
+
+    char* end;
+    end = A;
+    i = -1;
+    new->coordinates = (double*)malloc(sizeof(double) * 100);
+    if (new->coordinates == NULL) {
+        printf("Alloceted error");
+
+        return 1;
+    }
+
+    while (*A) {
+        new->coordinates[i] = strtod(A, &end);
+        A = end;
+        i++;
+        while (!(isdigit(*A) || *A == '-' || *A == '+') && *A) {
+            A++;
+        }
+    }
+    new->size = i;
+
+    return 0;
 }
